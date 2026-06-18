ca4d57b2d315a34208b86838cf525cc05c6693fc
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-18 15:29:13 -0500

    Add RunContext class to base.py

--- tx_fwi/components/base.py
+++ tx_fwi/components/base.py
@@ -1,1 +1,6 @@
-
+# base.py
+class RunContext:
+    def __init__(self, storage, registry, upstream):
+        self.storage = storage
+        self.registry = registry
+        self.upstream = upstream
