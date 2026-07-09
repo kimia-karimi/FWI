5585fe65e9f2d15b8802facf559a9dee9e991f0f
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-07-09 12:33:46 -0500

    Add DiversionflowComponent and ReturnFlowComponent imports

--- tx_fwi/runner.py
+++ tx_fwi/runner.py
@@ -12,8 +12,8 @@ from tx_fwi.components.base import RunContext
 # Components
 from tx_fwi.components.gaged import LocalGagedComponent
 from tx_fwi.components.gaged_upstream import UpstreamGagedComponent
-
-
+from tx_fwi.components.diversion import DiversionflowComponent
+from tx_fwi.components.return import ReturnFlowComponent
 # ------------------------------------------------------------------
 # DEFAULT PATHS (UNC-friendly)
 # ------------------------------------------------------------------
