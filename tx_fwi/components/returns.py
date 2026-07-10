c1ffd83bf624e3fdd2967abab2ccded3d1991375
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-07-10 09:21:59 -0500

    Add import for io module in returns.py

--- tx_fwi/components/returns.py
+++ tx_fwi/components/returns.py
@@ -7,6 +7,7 @@ import requests
 from tx_fwi.components.base import RunContext
 from tx_fwi.sources.base import registry
 from tx_fwi.transforms.units import mgd_to_afday
+import io
 REQUIRED_OUT_COLS = [
     "date",
     "id",
