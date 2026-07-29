e096a62240c42702cdde939ba502df347478dcf8
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-07-29 13:06:20 -0500

    Replace load_daily_model_output with load_txrr_flow

--- tx_fwi/sources/__init__.py
+++ tx_fwi/sources/__init__.py
@@ -1,12 +1,12 @@
 from .usgs import fetch_usgs_daily_afday, fetch_usgs_daily_cfs
 from .ibwc import fetch_ibwc_daily_rounded_afday
 from .lnra import load_lake_texana_afday
-from .models import load_daily_model_output
+from .models import load_txrr_flow
 
 __all__ = [
     "fetch_usgs_daily_afday",
     "fetch_usgs_daily_cfs",
     "fetch_ibwc_daily_rounded_afday",
     "load_lake_texana_afday",
-    "load_daily_model_output",
+    "load_txrr_flow",
 ]
