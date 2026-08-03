b46212cf39f73384096946e3263d3be33a45246d
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-08-03 13:20:52 -0500

    Update gaged.py

--- tx_fwi/components/gaged.py
+++ tx_fwi/components/gaged.py
@@ -50,7 +50,7 @@ class LocalGagedComponent:
             )
 
         print(f"[Local] source={source_norm}, special={special_norm}, gage={gage_id}")
-        print(registry._handlers.keys())
+        #print(registry._handlers.keys())
 
 
         #  unified calling convention
