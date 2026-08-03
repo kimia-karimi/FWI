0b14ce289555a70c2534ccbe6264c532cc77c2da
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-08-03 12:46:17 -0500

    Update __init__.py

--- tx_fwi/sources/__init__.py
+++ tx_fwi/sources/__init__.py
@@ -2,11 +2,14 @@ from .usgs import fetch_usgs_daily_afday, fetch_usgs_daily_cfs
 from .ibwc import fetch_ibwc_daily_rounded_afday
 from .lnra import load_lake_texana_afday
 from .models import load_txrr_flow
-
+from .lake_houston import lake_houston_afday
+from .colorado import colorado_adjusted_afday
 __all__ = [
     "fetch_usgs_daily_afday",
     "fetch_usgs_daily_cfs",
     "fetch_ibwc_daily_rounded_afday",
     "load_lake_texana_afday",
     "load_txrr_flow",
+    "lake_houston_afday",
+    "colorado_adjusted_afday"
 ]
