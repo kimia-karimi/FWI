2de1f82b6f1471862526302acad409d97154509d
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-08-06 08:43:10 -0500

    Update runner.py

--- tx_fwi/runner.py
+++ tx_fwi/runner.py
@@ -109,30 +109,30 @@ def run_all(start: str | None = None, end: str | None = None):
     # --------------------------------------------------------------
     # Component 3: Ungaged flow 
     # --------------------------------------------------------------
-    #print("\nRunning ungaged flow component...")
+    print("\nRunning ungaged flow component...")
 
-    #comp_ungaged = UngagedComponent(ctx)
-    #n_ungaged = comp_ungaged.run(start=start_dt, end=end_dt)
-    #print(f"Ungaged rows added: {n_ungaged:,}")
+    comp_ungaged = UngagedComponent(ctx)
+    n_ungaged = comp_ungaged.run(start=start_dt, end=end_dt)
+    print(f"Ungaged rows added: {n_ungaged:,}")
     
     # --------------------------------------------------------------
     # Component 4: Diversion 
     # --------------------------------------------------------------
-    #print("\nRunning diversion component...")
+    print("\nRunning diversion component...")
 
-    #comp_diversion = DiversionflowComponent(ctx)
-    #n_diversion = comp_diversion.run(start=start_dt, end=end_dt)
+    comp_diversion = DiversionflowComponent(ctx)
+    n_diversion = comp_diversion.run(start=start_dt, end=end_dt)
 
-    #print(f"Diversion rows added: {n_diversion:,}")
+    print(f"Diversion rows added: {n_diversion:,}")
     # --------------------------------------------------------------
     # Component 5: Return 
     # --------------------------------------------------------------
-    #print("\nRunning return component...")
+    print("\nRunning return component...")
 
-    #comp_return = ReturnFlowComponent(ctx)
-    #n_return = comp_return.run(start=start_dt, end=end_dt)
+    comp_return = ReturnFlowComponent(ctx)
+    n_return = comp_return.run(start=start_dt, end=end_dt)
     
-    #print(f"Return rows added: {n_return:,}")
+    print(f"Return rows added: {n_return:,}")
     print("\nPipeline complete.")
 
 
