49d57cce0fd2e27349899c10cae59b574a9266c8
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-08-20 12:15:40 -0500

    Update returns.py

--- tx_fwi/components/returns.py
+++ tx_fwi/components/returns.py
@@ -377,7 +377,7 @@ class ReturnComponent:
         watersheds = (
             self.ctx.registry
             .load_watersheds()
-            [["WS_ID", "Estuary", "geometry"]]
+            [["WS_ID", "EST_GROUP", "geometry"]]
         )
 
         watersheds = watersheds.to_crs(
@@ -417,7 +417,7 @@ class ReturnComponent:
             .groupby(
                 [
                     "WS_ID",
-                    "Estuary",
+                    "EST_GROUP",
                     "year",
                     "month",
                 ],
