43de65307af23fc9360bd85777a88137327a01e9
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-08-20 11:20:35 -0500

    Update epa_dmr.py

--- tx_fwi/sources/epa_dmr.py
+++ tx_fwi/sources/epa_dmr.py
@@ -41,7 +41,7 @@ class ReturnFlowSource:
     # Download DMR ZIP
     # --------------------------------------------------
     def _download_fy_zip(self, fy):
-
+        STATE = "TX"
         fname = f"{STATE}_FY{fy}_NPDES_DMRS_LIMITS.zip"
 
         url = f"{BASE_URL}/{fname}"
