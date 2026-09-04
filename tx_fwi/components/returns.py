0b22abcba925b80f55b4ff4c547f4dd907d85918
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-09-04 12:01:35 -0500

    Add data_as_of timestamp to daily returns

--- tx_fwi/components/returns.py
+++ tx_fwi/components/returns.py
@@ -490,6 +490,7 @@ class ReturnComponent:
         daily["flow_role"] = "return"
 
         daily["count_in_basin_sum"] = 1
+        df["data_as_of"] = pd.Timestamp(end)
 
         daily["note"] = None
 
