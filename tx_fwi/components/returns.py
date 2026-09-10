9c93c383d5799bfd1ee67cc5e68197da87f68a34
Kimia Karimi <kim.karimi@twdb.texas.gov>
2026-09-10 16:13:12 -0500

    Update returns.py

--- tx_fwi/components/returns.py
+++ tx_fwi/components/returns.py
@@ -527,16 +527,16 @@ class ReturnComponent:
 
         if df.empty:
             print("[ReturnFlow] No rows written")
-            return 0
+          
 
-        n = self.ctx.storage.append(df)
+        #n = self.ctx.storage.append(df)
 
-        self.ctx.storage.set_watermark(
-            self.name,
-            df["date"].max(),
-        )
+        #self.ctx.storage.set_watermark(
+            #self.name,
+           # df["date"].max(),
+       # )
 
-        return n
+        return df
         
 def build_return(monthly_df):
 
